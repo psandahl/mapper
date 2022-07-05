6bbacc33cf5d2aefecc90cbb78ef9bf463bbb09e
Patrik Sandahl <patrik.sandahl@gmail.com>
2022-07-05 22:28:07 +0200

    Skeleton for mapping.

--- mapper/__main__.py
+++ mapper/__main__.py
@@ -10,16 +10,17 @@ from mapper.util.panel import Panel
 
 import mapper.vision.matrix as mat
 import mapper.vision.keypoint as kp
+import mapper.vision.mapping as map
 import mapper.vision.tracking as trk
 import mapper.vision.transform as trf
 
 
-def print_pose_comparision(pose: np.ndarray, gt_pose: np.ndarray) -> None:
+def print_pose_comparision(label: str, pose: np.ndarray, gt_pose: np.ndarray) -> None:
     ypr, t = mat.decomp_pose_matrix_yprt_yxz(pose)
     ypr_gt, t_gt = mat.decomp_pose_matrix_yprt_yxz(gt_pose)
     dist = np.linalg.norm(t - t_gt)
 
-    print('Pose comparision')
+    print(f'Pose comparision - {label}')
     print(' Position:')
     print(f'  VO x={t[0]:+.2f} y={t[1]:+.2f} z={t[2]:+.2f}')
     print(f'  GT x={t_gt[0]:+.2f} y={t_gt[1]:+.2f} z={t_gt[2]:+.2f}')
@@ -76,12 +77,12 @@ def tracking(data_dir: str) -> None:
             rel_pose, pose_match = trk.visual_pose_prediction(match,
                                                               instrinsic_matrix)
             print(
-                f"Number of pose inliers={len(pose_match['query_keypoints'])}")
+                f"Number of pose prediction inliers={len(pose_match['query_keypoints'])}")
 
             pose = trf.change_pose(prev_pose, rel_pose)
             poses.append(pose)
 
-            print_pose_comparision(pose, gt_pose)
+            print_pose_comparision('prediction', pose, gt_pose)
 
             panel.set_caption(f'frame={frame_id}')
             panel.set_pose_matches(prev_image,
@@ -117,13 +118,122 @@ def tracking(data_dir: str) -> None:
     panel.destroy_window()
 
 
+def tracking_and_mapping(data_dir: str, cheat_frames: int = 1) -> None:
+    kp.configure_keypoint(kp.KeypointType.AKAZE)
+
+    data_extent = misc.read_2d_box_from_3x4_matrices(
+        os.path.join(data_dir, 'poses.txt'))
+
+    panel = Panel(data_extent)
+
+    images = list()
+    intrinsic_matrices = list()
+    descriptor_pairs = list()
+    pose_matches = list()
+    gt_poses = list()
+    poses = list()
+
+    # Iterate through dataset.
+    for image, proj_matrix, gt_pose in kd.KittiData(data_dir):
+        frame_id = len(images)
+
+        print(f'Processing frame={frame_id}')
+
+        # Unpack the intrinsic matrix.
+        instrinsic_matrix, _ = mat.decomp_pose_matrix(proj_matrix)
+
+        # Compute keypoints and descriptors.
+        frame_keypoints = kp.detect(image, 650)
+        frame_descriptor_pair = kp.compute(image, frame_keypoints)
+
+        if frame_id > 0:
+            # Do stuff.
+            prev_image = images[-1]
+            prev_pose = poses[-1]
+            prev_descriptor_pair = descriptor_pairs[-1]
+
+            # Get a keypoint match with the previous frame.
+            match = kp.match(prev_descriptor_pair, frame_id - 1,
+                             frame_descriptor_pair, frame_id)
+            print(
+                f"Number of matching keypoints={len(match['query_keypoints'])}")
+
+            scale = 1.0
+            if frame_id <= cheat_frames:
+                # To get the first triangulations right, bootstrap with
+                # help of the ground truth.
+                scale = pose_distance(gt_poses[-1], gt_pose)
+                print(f'Cheating with scale from GT={scale}')
+
+            # Using the match, predict the pose for this frame.
+            rel_pose, pose_match = trk.visual_pose_prediction(match,
+                                                              instrinsic_matrix,
+                                                              scale=scale)
+
+            print(
+                f"Number of pose prediction inliers={len(pose_match['query_keypoints'])}")
+
+            pose = trf.change_pose(prev_pose, rel_pose)
+
+            print_pose_comparision('prediction', pose, gt_pose)
+
+            # If there's at least two previous frames, we can map from the history
+            # and do a better estimation for the current frame using the map.
+            if len(images) > 1:
+                print('Map landmarks using history ...')
+
+                # Note: This requires adjacent frames atm.
+                map.sparse_mapping(frame_id,
+                                   poses[-2], intrinsic_matrices[-2], images[-2],
+                                   poses[-1], intrinsic_matrices[-1], images[-1],
+                                   pose_matches[-1])
+
+            panel.set_caption(f'frame={frame_id}')
+            panel.set_pose_matches(prev_image,
+                                   cv.KeyPoint_convert(
+                                       prev_descriptor_pair[0]),
+                                   cv.KeyPoint_convert(
+                                       pose_match['train_keypoints']),
+                                   image,
+                                   cv.KeyPoint_convert(
+                                       frame_descriptor_pair[0]),
+                                   cv.KeyPoint_convert(pose_match['query_keypoints']))
+
+            panel.add_camera(gt_pose)
+            panel.add_camera(pose, color=(255, 0, 0))
+            panel.update()
+
+            # Update lists with pose match with previous frame and the pose.
+            pose_matches.append(pose_match)
+            poses.append(pose)
+
+            key = cv.waitKey(30)
+            if key == 27:
+                break
+        else:
+            # This is the first frame. Use the ground truth pose.
+            pose_matches.append(None)
+            poses.append(gt_pose)
+
+        # Save stuff.
+        images.append(image)
+        intrinsic_matrices.append(instrinsic_matrix)
+        descriptor_pairs.append(frame_descriptor_pair)
+        gt_poses.append(gt_pose)
+
+    print('Track is complete. Press key + ENTER to quit')
+    sys.stdin.read(1)
+
+    panel.destroy_window()
+
+
 def main():
     # tracking('C:\\Users\\patri\\kitti\\KITTI_sequence_1')
-    tracking('C:\\Users\\patri\\kitti\\KITTI_sequence_2')
+    # tracking('C:\\Users\\patri\\kitti\\KITTI_sequence_2')
     # tracking('C:\\Users\\patri\\kitti\\KITTI_sequence_long_1')
     # tracking('C:\\Users\\patri\\kitti\\parking\\parking')
 
-    # tracking_and_mapping('C:\\Users\\patri\\kitti\\KITTI_sequence_2')
+    tracking_and_mapping('C:\\Users\\patri\\kitti\\KITTI_sequence_2')
 
 
 if __name__ == '__main__':
--- mapper/vision/mapping.py
+++ mapper/vision/mapping.py
@@ -1,12 +1,27 @@
 import numpy as np
 import cv2 as cv
 
+import mapper.vision.image as im
 
-def sparse_mapping(pose_0: np.ndarray,
-                   instrinsic_0: np.ndarray,
+
+def sparse_mapping(frame_id: int,
+                   pose_0: np.ndarray,
+                   intrinsic_0: np.ndarray,
                    image_0: np.ndarray,
                    pose_1: np.ndarray,
-                   instrinsic_1: np.ndarray,
+                   intrinsic_1: np.ndarray,
                    image_1: np.ndarray,
                    match: dict) -> None:
-    ()
+    assert isinstance(frame_id, int)
+    assert frame_id > 0
+    assert isinstance(pose_0, np.ndarray)
+    assert pose_0.shape == (3, 4)
+    assert isinstance(intrinsic_0, np.ndarray)
+    assert intrinsic_0.shape == (3, 3)
+    assert im.is_image(image_0)
+    assert isinstance(pose_1, np.ndarray)
+    assert pose_1.shape == (3, 4)
+    assert isinstance(intrinsic_1, np.ndarray)
+    assert intrinsic_1.shape == (3, 3)
+    assert im.is_image(image_1)
+    assert isinstance(match, dict)
